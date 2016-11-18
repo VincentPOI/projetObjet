57b056e603aa88db906fec265d7ba65fe202957e
hmilet <hadrien.milet@hotmail.fr>
2016-11-18 15:17:36 +0100

    Ajout FauteuilRoulant.cpp

--- FauteuilRoulant.cpp
+++ FauteuilRoulant.cpp
@@ -13,12 +13,31 @@
 
 #include "FauteuilRoulant.h"
 
-FauteuilRoulant::FauteuilRoulant() {
+FauteuilRoulant::FauteuilRoulant():Article() {
+    this->largeurAssise=NULL;
+    this->poidsFauteuil=NULL;            
 }
 
-FauteuilRoulant::FauteuilRoulant(const FauteuilRoulant& orig) {
+FauteuilRoulant::FauteuilRoulant(string refArticle,string marqueArticle,string modeleArticle, double prixJourArticle,int nbStockarticle, double largeurAssise, double poidsFauteuil):Article(refArticle,marqueArticle,modeleArticle,prixJourArticle,nbStockarticle) {
+    this->largeurAssise=largeurAssise;
+    this->poidsFauteuil=poidsFauteuil;
 }
 
 FauteuilRoulant::~FauteuilRoulant() {
 }
 
+void FauteuilRoulant::setLargeurAssise(double largeurAssise){
+    this->largeurAssise=largeurAssise;
+}
+
+double FauteuilRoulant::getLargeurAssise(){
+    return this->largeurAssise;
+}
+
+void FauteuilRoulant::setPoidsFauteuil(double poidsFauteuil){
+    this->poidsFauteuil=poidsFauteuil;
+}
+
+double FauteuilRoulant::getPoidsFauteuil(){
+    return this->poidsFauteuil;
+}
